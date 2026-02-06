db4066a61ef6076dc7d53cfc81dea12d6e41c446
appletun091305 <lauliu26@bergen.org>
2026-02-06 13:53:02 -0500

    data (change to proj e)

---
+++ DataAnalyzer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+class DataAnalyzer {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("countries.txt"));
+    }
+}
